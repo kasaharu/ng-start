d4972ee68273d0af3d938debfc4a5896ca072cdd
Wataru.Kasahara <kasahara1019@gmail.com>
2020-06-24 22:16:03 +0900

    yarn ng g c product-list

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProductListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
