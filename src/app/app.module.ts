c5bf32d01db80917f67abf9f5a9750ebd8bcd014
Wataru.Kasahara <kasahara1019@gmail.com>
2020-06-24 22:34:57 +0900

    feat(ajp): start-routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, TopBarComponent, ProductAlertsComponent],
+  declarations: [AppComponent, ProductListComponent, TopBarComponent, ProductAlertsComponent, ProductDetailsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
