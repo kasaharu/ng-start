fc8eb52cd3b40b1a81b3a1f1ceda0acd76693779
Wataru.Kasahara <kasahara1019@gmail.com>
2020-06-24 22:31:28 +0900

    feat(ajp): start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, TopBarComponent],
+  declarations: [AppComponent, ProductListComponent, TopBarComponent, ProductAlertsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,14 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.scss'],
+})
+export class ProductAlertsComponent implements OnInit {
+  @Input() product: any;
+  @Output() notify = new EventEmitter();
+  constructor() {}
+
+  ngOnInit() {}
+}
